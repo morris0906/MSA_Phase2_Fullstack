1e665abecc1e4c0fa60c849261f02021c96aa18b
Han <morrs456@gmail.com>
2020-11-17 04:51:46 +1300

    change

---
+++ react-blog/src/components/Post.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Post = () =>{
+    return(
+        <div className="post">
+            <header className="postheader">
+                <h1>
+                    Post Page.
+                </h1>
+            </header>
+        </div>
+    );
+
+};
+
+export default Post;
